7b11e8500e453304e99a53ab219fa5800ae147ea
Kis Lilla Laura <142574961+KisLillaLaura@users.noreply.github.com>
2025-05-17 23:50:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ let moleculesData = [];
 
 async function loadMoleculesData() {
     try {
-        const response = await fetch('molekulak.json');
+        const response = await fetch('gyogykemsumma_dict.json');
         if (!response.ok) {
             throw new Error('Hiba a JSON betöltésekor');
         }

75b66fc12b77222373d424ea5577aff471688925
Kis Lilla Laura <142574961+KisLillaLaura@users.noreply.github.com>
2025-05-18 12:47:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,10 @@ function initializeGroupSelect() {
     groupSelect.appendChild(option);
   });
 }
+function getRandomElements(arr, n) {
+  const shuffled = [...arr].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, n);
+}
 
 function setupEventListeners() {
   document.getElementById("beugroBtn").addEventListener("click", () => {
